43b325eb6ef3077c4f60d75483344a0c0955676c
SamantaQA2 <sanjidaaktersamanta02@gmail.com>
2025-11-12 11:06:37 +0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -91,8 +91,8 @@ export default defineConfig([
       "@typescript-eslint/no-floating-promises": "warn", // Require proper promise handling
       "@typescript-eslint/no-misused-promises": "error", // Disallow misused promises
       "@typescript-eslint/consistent-type-imports": "warn", // Enforce using `import type`
-      "@typescript-eslint/array-type": "warn", // Enforce consistent array type syntax
-      "@typescript-eslint/no-shadow": "error", // Disallow variable shadowing
+      // "@typescript-eslint/array-type": "warn", // Enforce consistent array type syntax
+      //"@typescript-eslint/no-shadow": "error", // Disallow variable shadowing
       // "@typescript-eslint/ban-types": "error", // Disallow certain built-in types
       "@typescript-eslint/no-unsafe-argument": "warn", // Disallow unsafe arguments
       "@typescript-eslint/no-unsafe-assignment": "warn", // Disallow unsafe assignments
@@ -100,3 +100,4 @@ export default defineConfig([
     },
   },
 ]);
+
